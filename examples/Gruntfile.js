be5c2327a8863dc7e31accc4cc2300ab33c48be3
Mark Daggett <mark@humansized.com>
2014-02-16 12:09:58 -0600

    working on getting the server up and running again, not just the build.

--- examples/Gruntfile.js
+++ examples/Gruntfile.js
@@ -8,125 +8,202 @@
  */
 
 'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
 
 // # Globbing
 // for performance reasons we're only matching one level down:
-// '<%= config.src %>/templates/pages/{,*/}*.hbs'
+// '<%= app.src %>/templates/pages/{,*/}*.hbs'
 // use this if you want to match all subfolders:
-// '<%= config.src %>/templates/pages/**/*.hbs'
+// '<%= app.src %>/templates/pages/**/*.hbs'
 
 module.exports = function(grunt) {
 
   require('time-grunt')(grunt);
   require('load-grunt-tasks')(grunt, {pattern: ['grunt-*', 'assemble']});
 
+  var appConfig = {
+    src: 'src',
+    dist: 'dist'
+  };
+
+  try {
+    appConfig.src = require('./bower.json').appPath || appConfig.src;
+  } catch (e) {}
+
+
   // Project configuration.
   grunt.initConfig({
-
-    config: {
-      src: 'src',
-      dist: 'dist'
-    },
-
+    app: appConfig,
     watch: {
       assemble: {
-        files: ['<%= config.src %>/{content,data,templates}/**/*.{md,hbs,yml}', '<%= config.src %>/../../docs/**/*.{md,hbs,yml}'],
+        files: ['<%= app.src %>/{content,data,templates}/**/*.{md,hbs,yml}', '<%= app.src %>/../../docs/**/*.{md,hbs,yml}'],
         tasks: ['assemble']
       },
       livereload: {
         options: {
-          livereload: '<%= connect.options.livereload %>'
+          livereload: LIVERELOAD_PORT
         },
         files: [
-          '<%= config.dist %>/{,*/}*.html',
-          '<%= config.dist %>/assets/{,*/}*.css',
-          '<%= config.dist %>/assets/{,*/}*.js',
-          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+          '{.tmp,<%= app.src %>}/**/*.html',
+          '.tmp/assets/{,*/}*.css',
+          '<%= app.dist %>/{,*/}*.html',
+          '{.tmp,<%= app.src %>}/scripts/**/*.js',
+          '<%= app.src %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
       }
     },
     connect: {
       options: {
         port: 9000,
-        livereload: 35729,
         // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
-          open: true,
-          base: [
-            '<%= config.dist %>'
-          ]
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, appConfig.src)
+            ];
+          }
+        },
+        dist: {
+          options: {
+            middleware: function (connect) {
+              return [
+                mountFolder(connect, appConfig.dist)
+              ];
+            }
+          }
         }
       }
     },
+    open: {
+      server: {
+        url: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>'
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= app.src %>',
+          dest: '<%= app.dist %>',
+          src: [
+            '*.{ico,txt}',
+            '.htaccess',
+            'scripts/data/{,*/}*.json',
+            'images/{,*/}*.{webp,gif}',
+            'assets/css/fonts/{,*/}*.*'
+          ]
+        }, {
+          expand: true,
+          cwd: '.tmp/images',
+          dest: '<%= app.dist %>/images',
+          src: [
+            'generated/*'
+          ]
+        }]
+      },
+      styles: {
+        expand: true,
+        cwd: '<%= app.src %>/assets/css',
+        dest: '.tmp/assets/css/',
+        src: '{,*/}*.css'
+      }
+    },
+    concurrent: {
+      server: [
+        'copy:styles'
+      ],
+      test: [
+        'copy:styles'
+      ],
+      dist: [
+        'copy:styles'
+      ]
+    },
 
     useminPrepare: {
-      html: '<%= config.dist %>/index.html',
+      html: '<%= app.src %>/index.html',
       options: {
-        dest: '<%= config.dist %>'
+        dest: '<%= app.dist %>'
       }
     },
     usemin: {
-      html: ['<%= config.dist %>/**/*.html'],
-      css: ['<%= config.dist %>/assets/css/{,*/}*.css'],
+      html: ['<%= app.dist %>/**/*.html'],
+      css: ['<%= app.dist %>/assets/css/{,*/}*.css'],
       options: {
-        dirs: ['<%= config.dist %>']
+        dirs: ['<%= app.dist %>']
       }
     },
-
-
-
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= app.dist %>/*',
+            '!<%= app.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+    cssmin: {
+    },
     assemble: {
       pages: {
         options: {
           flatten: true,
-          assets: '<%= config.dist %>/assets',
-          layout: '<%= config.src %>/templates/layouts/default.hbs',
-          data: '<%= config.src %>/data/*.{json,yml}',
-          partials: '<%= config.src %>/templates/partials/*.hbs',
+          assets: '<%= app.dist %>/assets',
+          layout: '<%= app.src %>/templates/layouts/default.hbs',
+          data: '<%= app.src %>/data/*.{json,yml}',
+          partials: '<%= app.src %>/templates/partials/*.hbs',
           plugins: ['assemble-contrib-permalinks', 'assemble-contrib-sitemap']
         },
         files: {
-          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs'],
-          '<%= config.dist %>/charts/column/': ['<%= config.src %>/content/charts/column/*.hbs'],
-          '<%= config.dist %>/charts/grouped-column/': ['<%= config.src %>/content/charts/grouped-column/*.hbs'],
-          '<%= config.dist %>/charts/line/': ['<%= config.src %>/content/charts/line/*.hbs'],
-          '<%= config.dist %>/charts/row/': ['<%= config.src %>/content/charts/row/*.hbs'],
-          '<%= config.dist %>/charts/scatter/': ['<%= config.src %>/content/charts/scatter/*.hbs'],
-          '<%= config.dist %>/charts/stacked-column/': ['<%= config.src %>/content/charts/stacked-column/*.hbs'],
-          '<%= config.dist %>/charts/waterfall/': ['<%= config.src %>/content/charts/waterfall/*.hbs']
+          '<%= app.dist %>/': ['<%= app.src %>/templates/pages/*.hbs'],
+          '<%= app.dist %>/charts/column/': ['<%= app.src %>/content/charts/column/*.hbs'],
+          '<%= app.dist %>/charts/grouped-column/': ['<%= app.src %>/content/charts/grouped-column/*.hbs'],
+          '<%= app.dist %>/charts/line/': ['<%= app.src %>/content/charts/line/*.hbs'],
+          '<%= app.dist %>/charts/row/': ['<%= app.src %>/content/charts/row/*.hbs'],
+          '<%= app.dist %>/charts/scatter/': ['<%= app.src %>/content/charts/scatter/*.hbs'],
+          '<%= app.dist %>/charts/stacked-column/': ['<%= app.src %>/content/charts/stacked-column/*.hbs'],
+          '<%= app.dist %>/charts/waterfall/': ['<%= app.src %>/content/charts/waterfall/*.hbs']
         }
       }
     },
+  });
+  grunt.registerTask('server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+    }
 
-    // Before generating any new files,
-    // remove any previously-created files.
-    clean: ['<%= config.dist %>/**/*.{html,xml}']
-
+    grunt.task.run([
+      'clean:server',
+      'assemble',
+      'concurrent:server',
+      'autoprefixer',
+      'connect:livereload',
+      'open',
+      'watch'
+    ]);
   });
 
-  // grunt.loadNpmTasks('assemble');
-  // grunt.loadNpmTasks('grunt-contrib-clean');
-  // grunt.loadNpmTasks('grunt-usemin');
-  // grunt.loadNpmTasks('grunt-contrib-connect');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('server', [
-    'clean',
-    'assemble',
-    'connect:livereload',
-    'watch'
-  ]);
-
   grunt.registerTask('build', [
-    'clean',
+    'clean:dist',
     'assemble',
     'useminPrepare',
-    'concat',
+    'copy:dist',
     'usemin',
-    'uglify'
   ]);
 
   grunt.registerTask('default', [
