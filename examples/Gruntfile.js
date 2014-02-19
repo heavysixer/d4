a4877f68adba32ac6a5284992a568f826638df3b
Mark Daggett <mark@humansized.com>
2014-02-19 14:45:39 -0600

    fixing the grunt file.

--- examples/Gruntfile.js
+++ examples/Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function(grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 3000,
         // change this to '0.0.0.0' to access the server from outside
         hostname: '0.0.0.0'
       },
