dc024a553ffdccacfbf3e6a3e286cc2b537ad2c5
Mark Daggett <mark@humansized.com>
2014-02-26 11:27:57 -0600

    Add some basic conformational tests for the nested-group parser.

--- test/tests/parsers/nested-group-test.js
+++ test/tests/parsers/nested-group-test.js
@@ -13,9 +13,35 @@ describe('d4.parsers.nestedGroup', function() {
 
   it('should use default accessors if none or provided', function(){
     var parsedData = d4.parsers.nestedGroup()(this.data);
-
+    expect(parsedData.x.key).to.equal('x');
+    expect(parsedData.y.key).to.equal('y');
+    expect(parsedData.value.key).to.equal('value');
+    expect(parsedData.data[0].values.length).to.equal(2);
   });
-  it('should allow functions or primitives to be used as accessors', function(){
 
+  it('should allow functions or primitives to be used as accessors', function(){
+    var parsedData = d4.parsers.nestedGroup()
+    .x('a')
+    .y(function(){
+      return 'b';
+    })
+    (this.data);
+    expect(parsedData.x.key).to.equal('a');
+    expect(parsedData.data[0].key).to.equal('10');
+    expect(parsedData.y.key).to.equal('b');
+    expect(parsedData.value.key).to.equal('value');
+  });
+
+  it('should allow you to specify a nestKey which can be used to sort on a dimension', function(){
+    var parsedData = d4.parsers.nestedGroup()
+    .x('a')
+    .y(function(){
+      return 'b';
+    })
+    .nestKey('c')
+    (this.data);
+    expect(parsedData.data.length).to.equal(2);
+    expect(parsedData.data[0].key).to.equal('30');
+    expect(parsedData.value.key).to.equal('value');
   });
 });
