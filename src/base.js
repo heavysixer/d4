fb2682de8647c3117617ded74b35faa4922719d7
Mark Daggett <mark@humansized.com>
2014-03-19 10:22:56 -0500

    Remove the padding option, I could not find a compelling reason to use it.

--- src/base.js
+++ src/base.js
@@ -289,17 +289,11 @@
         bottom: 20,
         left: 40
       },
-      padding: {
-        top: 20,
-        left: 20,
-        right: 20,
-        bottom: 20
-      }
     }, config);
 
     linkAxes(opts);
     assignDefaultBuilder.bind(opts)(defaultBuilder || builder);
-    opts.accessors = ['margin', 'padding', 'width', 'height', 'valueKey'].concat(config.accessors || []);
+    opts.accessors = ['margin', 'width', 'height', 'valueKey'].concat(config.accessors || []);
     return opts;
   };
 
@@ -350,21 +344,16 @@
   };
 
   var scaffoldChart = function(selection, data) {
-    var innerWidth = this.width - this.margin.left - this.margin.right;
-    var innerHeight = this.height - this.margin.top - this.margin.bottom;
-    this.width = innerWidth - this.padding.left - this.padding.right;
-    this.height = innerHeight - this.padding.top - this.padding.bottom;
+    this.width = this.width - this.margin.left - this.margin.right;
+    this.height = this.height - this.margin.top - this.margin.bottom;
     this.svg = d3.select(selection).selectAll('svg').data([data]);
     this.featuresGroup = this.svg.enter().append('svg')
     .append('g')
-    .attr('class', 'container')
-    .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
-    .append('g')
       .attr('class', 'featuresGroup')
-      .attr('transform', 'translate(' + this.padding.left + ',' + this.padding.top + ')');
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
     this.svg
-    .attr('width', this.width + this.padding.left + this.padding.right + this.margin.left + this.margin.right)
-    .attr('height', this.height + this.padding.top + this.padding.bottom + this.margin.top + this.margin.bottom)
+    .attr('width', this.width + this.margin.left + this.margin.right)
+    .attr('height', this.height + this.margin.top + this.margin.bottom)
     .attr('class', 'd4');
     this.svg.append('defs');
   };
@@ -533,7 +522,6 @@
     var opts = assignDefaults(options && options.config || {}, options && options.builder || undefined);
     var chart = applyScaffold(opts);
     createAccessorsFromArray(chart, opts.margin, d3.keys(opts.margin), 'margin');
-    createAccessorsFromArray(chart, opts.padding, d3.keys(opts.padding), 'padding');
 
     chart.accessors = opts.accessors;
     createAccessorsFromArray(chart, opts, chart.accessors);
