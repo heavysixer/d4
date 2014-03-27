f28b892b75f042f36a1f4175ae37d8c6142772bc
Mark Daggett <mark@humansized.com>
2014-03-27 12:27:52 -0500

    Update the stacked shaped features to include circles and ellipses too!

--- examples/src/assets/js/d4.js
+++ examples/src/assets/js/d4.js
@@ -2495,11 +2495,11 @@
 
 (function() {
   'use strict';
-  var sign = function(val){
+  var sign = function(val) {
     return (val > 0) ? 'positive' : 'negative';
   };
 
-  var useDiscretePosition = function(dimension, d){
+  var useDiscretePosition = function(dimension, d) {
     var axis = this[dimension];
     return axis(d[axis.$key]);
   };
@@ -2511,9 +2511,9 @@
 
   var useContinuousSize = function(dimension, d) {
     var axis = this[dimension];
-    if(typeof d.y0 !== 'undefined'){
+    if (typeof d.y0 !== 'undefined') {
       return Math.abs(axis(d.y0) - axis(d.y0 + d.y));
-    }else {
+    } else {
       return Math.abs(axis(d[axis.$key]) - axis(0));
     }
   };
@@ -2521,16 +2521,16 @@
   var useContinuousPosition = function(dimension, d) {
     var axis = this[dimension];
     var val;
-    if(typeof d.y0 !== 'undefined'){
-      if(dimension === 'y') {
+    if (typeof d.y0 !== 'undefined') {
+      if (dimension === 'y') {
         val = d.y0 + d.y;
-        return  val < 0 ? axis(d.y0) : axis(val);
+        return val < 0 ? axis(d.y0) : axis(val);
       } else {
         val = (d.y0 + d.y) - Math.max(0, d.y);
         return axis(val);
       }
     } else {
-      if(dimension === 'y') {
+      if (dimension === 'y') {
         return d[axis.$key] < 0 ? axis(0) : axis(d[axis.$key]);
       } else {
         val = d[axis.$key] - Math.max(0, d[axis.$key]);
@@ -2542,8 +2542,8 @@
   var baseShapeFeature = function(name, shapeType, renderShapeAttributes) {
     return {
       accessors: {
-        classes: function(d,i) {
-          return 'bar fill item'+ i + ' ' + sign(d[this.valueKey]) + ' ' + d[this.y.$key];
+        classes: function(d, i) {
+          return 'bar fill item' + i + ' ' + sign(d[this.valueKey]) + ' ' + d[this.y.$key];
         }
       },
 
@@ -2552,20 +2552,20 @@
 
         // create data join with the series data
         var group = this.svg.select('.' + name).selectAll('g')
-          .data(data, function(d, i){
+          .data(data, function(d, i) {
             return d.key + i;
           });
 
         group.enter().append('g')
           .attr('class', function(d, i) {
-            return 'series'+ i + ' ' +  this.y.$key;
+            return 'series' + i + ' ' + this.y.$key;
           }.bind(this));
         group.exit().remove();
 
         var shape = group.selectAll(shapeType)
-        .data(function(d) {
-          return d.values;
-        });
+          .data(function(d) {
+            return d.values;
+          });
 
         shape.enter().append(shapeType)
           .attr('class', d4.functor(scope.accessors.classes).bind(this));
@@ -2577,15 +2577,157 @@
     };
   };
 
- /**
-  * This feature is useful for displaying charts which need a series of rects.
-  * This feature can be used to display a sequential or stacked series of rects.
-  */
+  /**
+   * This feature is useful for displaying charts which need stacked circles.
+   * Note: Many of the d4 charts use the stacked series as the base, and simply
+   * renders only one series, if there is nothing to stack.
+   *
+   *##### Accessors
+   *
+   * `classes` - classes assigned to each circle in the series
+   * `cx` - placement on the chart's x axis
+   * `cy` - placement on the chart's y axis
+   * `r` - radius of the circle
+   */
+  d4.feature('circleSeries', function(name) {
+    var rectObj = {
+      accessors: {
+        cx: function(d) {
+          var size = 0;
+          if (this.x.$scale === 'ordinal') {
+            size = useDiscreteSize.bind(this)('x');
+            return useDiscretePosition.bind(this)('x', d) + size / 2;
+          } else {
+            size = useContinuousSize.bind(this)('x', d);
+            return useContinuousPosition.bind(this)('x', d) + size / 2;
+          }
+        },
+
+        cy: function(d) {
+          var size = 0;
+          if (this.y.$scale === 'ordinal') {
+            size = useDiscreteSize.bind(this)('y');
+            return useDiscretePosition.bind(this)('y', d) + size / 2;
+          } else {
+            size = useContinuousSize.bind(this)('y', d);
+            return useContinuousPosition.bind(this)('y', d) + size / 2;
+          }
+        },
+
+        r: function(d) {
+          var x, y;
+          if (this.x.$scale === 'ordinal') {
+            x = useDiscreteSize.bind(this)('x');
+          } else {
+            x = useContinuousSize.bind(this)('x', d);
+          }
+          if (this.y.$scale === 'ordinal') {
+            y = useDiscreteSize.bind(this)('y');
+          } else {
+            y = useContinuousSize.bind(this)('y', d);
+          }
+          return Math.min(x, y) / 2;
+        }
+      }
+    };
+
+    var renderShape = function(scope, selection) {
+      selection
+        .attr('r', d4.functor(scope.accessors.r).bind(this))
+        .attr('cx', d4.functor(scope.accessors.cx).bind(this))
+        .attr('cy', d4.functor(scope.accessors.cy).bind(this));
+    };
+    var baseObj = baseShapeFeature.bind(this)(name, 'circle', renderShape);
+    return d4.merge(baseObj, rectObj);
+  });
+
+  /**
+   * This feature is useful for displaying charts which need stacked ellipses.
+   * Note: Many of the d4 charts use the stacked series as the base, and simply
+   * renders only one series, if there is nothing to stack.
+   *
+   *##### Accessors
+   *
+   * `classes` - classes assigned to each ellipse in the series
+   * `cx` - placement on the chart's x axis
+   * `cy` - placement on the chart's y axis
+   * `rx` - radius of the ellipse on the x axis
+   * `ry` - radius of the ellipse on the y axis
+   */
+  d4.feature('ellipseSeries', function(name) {
+    var rectObj = {
+      accessors: {
+        cx: function(d) {
+          var size = 0;
+          if (this.x.$scale === 'ordinal') {
+            size = useDiscreteSize.bind(this)('x');
+            return useDiscretePosition.bind(this)('x', d) + size/2;
+          } else {
+            size = useContinuousSize.bind(this)('x', d);
+            return useContinuousPosition.bind(this)('x', d) + size/2;
+          }
+        },
+
+        cy: function(d) {
+          var size = 0;
+          if (this.y.$scale === 'ordinal') {
+            size = useDiscreteSize.bind(this)('y');
+            return useDiscretePosition.bind(this)('y', d) + size/2;
+          } else {
+            size = useContinuousSize.bind(this)('y', d);
+            return useContinuousPosition.bind(this)('y', d) + size/2;
+          }
+        },
+
+        rx: function(d) {
+          if (this.x.$scale === 'ordinal') {
+            return useDiscreteSize.bind(this)('x')/2;
+          } else {
+            return useContinuousSize.bind(this)('x', d)/2;
+          }
+        },
+
+        ry: function(d) {
+          if (this.y.$scale === 'ordinal') {
+            return useDiscreteSize.bind(this)('y')/2;
+          } else {
+            return useContinuousSize.bind(this)('y', d)/2;
+          }
+        }
+      }
+    };
+
+    var renderShape = function(scope, selection) {
+      selection
+        .attr('rx', d4.functor(scope.accessors.rx).bind(this))
+        .attr('ry', d4.functor(scope.accessors.ry).bind(this))
+        .attr('cx', d4.functor(scope.accessors.cx).bind(this))
+        .attr('cy', d4.functor(scope.accessors.cy).bind(this));
+    };
+    var baseObj = baseShapeFeature.bind(this)(name, 'ellipse', renderShape);
+    return d4.merge(baseObj, rectObj);
+  });
+
+  /**
+   * This feature is useful for displaying charts which need stacked rects.
+   * Note: Many of the d4 charts use the stacked series as the base, and simply
+   * renders only one series, if there is nothing to stack.
+   *
+   *##### Accessors
+   *
+   * `classes` - classes assigned to each rect in the series
+   * `height` - height of the rect
+   * `rx` -  rounding of the corners against the x dimension
+   * `ry` -  rounding of the corners against the y dimension
+   * `width` - width of the rect
+   * `x` - placement on the chart's x axis
+   * `y` - placement on the chart's y axis
+   */
   d4.feature('rectSeries', function(name) {
-    var rectObj =  {
+    var rectObj = {
       accessors: {
         height: function(d) {
-          if(this.y.$scale === 'ordinal'){
+          if (this.y.$scale === 'ordinal') {
             return useDiscreteSize.bind(this)('y');
           } else {
             return useContinuousSize.bind(this)('y', d);
@@ -2597,7 +2739,7 @@
         ry: 0,
 
         width: function(d) {
-          if(this.x.$scale === 'ordinal'){
+          if (this.x.$scale === 'ordinal') {
             return useDiscreteSize.bind(this)('x');
           } else {
             return useContinuousSize.bind(this)('x', d);
@@ -2605,15 +2747,15 @@
         },
 
         x: function(d) {
-          if(this.x.$scale === 'ordinal'){
+          if (this.x.$scale === 'ordinal') {
             return useDiscretePosition.bind(this)('x', d);
           } else {
-            return  useContinuousPosition.bind(this)('x', d);
+            return useContinuousPosition.bind(this)('x', d);
           }
         },
 
         y: function(d) {
-          if(this.y.$scale === 'ordinal'){
+          if (this.y.$scale === 'ordinal') {
             return useDiscretePosition.bind(this)('y', d);
           } else {
             return useContinuousPosition.bind(this)('y', d);
@@ -2623,17 +2765,16 @@
     };
     var renderShape = function(scope, selection) {
       selection
-      .attr('x', d4.functor(scope.accessors.x).bind(this))
-      .attr('y', d4.functor(scope.accessors.y).bind(this))
-      .attr('ry', d4.functor(scope.accessors.ry).bind(this))
-      .attr('rx', d4.functor(scope.accessors.rx).bind(this))
-      .attr('width', d4.functor(scope.accessors.width).bind(this))
-      .attr('height', d4.functor(scope.accessors.height).bind(this));
+        .attr('x', d4.functor(scope.accessors.x).bind(this))
+        .attr('y', d4.functor(scope.accessors.y).bind(this))
+        .attr('ry', d4.functor(scope.accessors.ry).bind(this))
+        .attr('rx', d4.functor(scope.accessors.rx).bind(this))
+        .attr('width', d4.functor(scope.accessors.width).bind(this))
+        .attr('height', d4.functor(scope.accessors.height).bind(this));
     };
     var baseObj = baseShapeFeature.bind(this)(name, 'rect', renderShape);
     return d4.merge(baseObj, rectObj);
   });
-
 }).call(this);
 
 (function() {
