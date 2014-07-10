53c4ef81d0cda2c4f6aa3d2491df7ec6ec67f8e4
Mark Daggett <mark@humansized.com>
2014-07-10 14:49:08 -0500

    Create the stub for a brush feature.

---
+++ src/features/brush.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  d4.feature('brush', function(name) {
+    var brush = d3.svg.brush();
+
+    var obj = {
+      accessors: {
+      },
+      proxies: [brush],
+      render: function(scope) {
+
+      }
+    };
+    return obj;
+  });
+}).call(this);
