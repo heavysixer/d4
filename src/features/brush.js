8414ac14f73bd157682379287aa21b7ca880f4f8
Mark Daggett <mark@humansized.com>
2014-07-11 15:00:18 -0500

    Additional work on the brush feature.

--- src/features/brush.js
+++ src/features/brush.js
@@ -6,19 +6,35 @@
 
     var obj = {
       accessors: {
-        brushstart : function(){},
-        brush: function(){},
-        brushend: function(){}
+        brushable: function() {
+          return d3.selectAll('.brushable');
+        },
+        brushstart: function() {
+          this.svg.classed('selecting', true);
+        },
+        brushmove: function() {
+          var e = d3.event.target.extent();
+          this.features[name].accessors.brushable().classed('selected', function(d) {
+            var selected = e[0][0] <= d[this.x.$key] &&
+            d[this.x.$key] <= e[1][0] &&
+            e[0][1] <= d[this.y.$key] &&
+            d[this.y.$key] <= e[1][1];
+            return selected;
+          }.bind(this));
+        },
+        brushend: function() {
+          this.svg.classed('selecting', !d3.event.target.empty());
+        }
       },
       proxies: [{
         target: brush
       }],
       render: function(scope, data, selection) {
         d4.appendOnce(selection, 'g.' + name)
-        .call(brush.x(this.x).y(this.y)
-        .on('brushstart', d4.functor(scope.accessors.brushstart).bind(this))
-        .on('brush', d4.functor(scope.accessors.brushmove).bind(this))
-        .on('brushend', d4.functor(scope.accessors.brushend).bind(this)));
+          .call(brush.x(this.x).y(this.y)
+            .on('brushstart', d4.functor(scope.accessors.brushstart).bind(this))
+            .on('brush', d4.functor(scope.accessors.brushmove).bind(this))
+            .on('brushend', d4.functor(scope.accessors.brushend).bind(this)));
       }
     };
     return obj;
