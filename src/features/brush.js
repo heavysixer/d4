fe500fe4de513a4038f51d8edf8e99b3ef2a33c1
Mark Daggett <mark@humansized.com>
2014-07-16 10:27:48 -0500

    rather than returning the brush object itself which will give you an awkward brush.brush.foo() we are passing along the missing function calls as accessors, which is more consistent with how the rest of d4 behaves.

--- src/features/brush.js
+++ src/features/brush.js
@@ -43,9 +43,6 @@
 
     var obj = {
       accessors: {
-        brush: function(obj) {
-          return obj;
-        },
         brushable: function() {
           return d3.selectAll('.brushable');
         },
@@ -60,6 +57,11 @@
         brushstart: function() {
           this.svg.classed('selecting', true);
         },
+        clamp : brush.clamp,
+        clear : brush.clear,
+        extent : brush.extent,
+        empty : brush.empty,
+        event : brush.event,
         selection: function(selection) {
           return selection;
         },
